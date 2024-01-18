f55b6212a9a94c080d229711f8af1efb65461ff5
killown <systemofdown@gmai.com>
2024-01-18 20:58:18 -0300

    code optimization, fix numpy stuff

--- src/plugins/dockbar.py
+++ src/plugins/dockbar.py
@@ -46,23 +46,30 @@ class Dockbar(Adw.Application):
 
     # Start the Dockbar application
     def do_start(self):
+        # Set up a timeout to periodically check process IDs
         GLib.timeout_add(300, self.check_pids)
-        
-        #populate self.stored_windows in the panel start
+    
+        # Populate self.stored_windows during panel start
         instance = Hyprland()
-        self.stored_windows = [i.address for i in instance.get_windows()]
-        
+        self.stored_windows = [i.pid for i in instance.get_windows() if i.pid != -1]
+    
+        # Read configuration from topbar toml
         with open(self.topbar_config, "r") as f:
             panel_toml = toml.load(f)
+    
+            # Iterate over panel configurations
             for p in panel_toml:
+                # Check if the panel is positioned on the left side
                 if "left" == p:
                     exclusive = panel_toml[p]["Exclusive"] == "True"
                     position = panel_toml[p]["position"]
+    
+                    # Create a left panel and associated components
                     self.left_panel = CreatePanel(
                         self, "LEFT", position, exclusive, 32, 0, "LeftBar"
                     )
-                    self.dockbar = self.utils.CreateFromAppList(self.dockbar_config,
-                        "v", "LeftBar", self.join_windows
+                    self.dockbar = self.utils.CreateFromAppList(
+                        self.dockbar_config, "v", "LeftBar", self.join_windows
                     )
                     self.add_launcher = Gtk.Button()
                     self.add_launcher.set_icon_name("tab-new-symbolic")
@@ -70,9 +77,13 @@ class Dockbar(Adw.Application):
                     self.dockbar.append(self.add_launcher)
                     self.left_panel.set_content(self.dockbar)
                     self.left_panel.present()
+    
+                # Check if the panel is positioned at the bottom
                 if "bottom" == p:
                     exclusive = panel_toml[p]["Exclusive"] == "True"
                     position = panel_toml[p]["position"]
+    
+                    # Create a bottom panel and associated components
                     self.bottom_panel = CreatePanel(
                         self, "BOTTOM", position, exclusive, 32, 0, "BottomBar"
                     )
@@ -84,118 +95,190 @@ class Dockbar(Adw.Application):
                     self.taskbar.add_css_class("taskbar")
                     self.bottom_panel.set_content(self.taskbar)
                     self.bottom_panel.present()
-                    #start the taskbar list first time, remaning check pids will do
-                    self.Taskbar("h", "taskbar")           
-  
+    
+                    # Start the taskbar list for the bottom panel
+                    # Remaining check pids will be handled later
+                    self.Taskbar("h", "taskbar")       
   
     def is_any_window_created_or_closed(self):
+        # Create an instance of the Hyprland class
         instance = Hyprland()
-        updated_windows = [i.address for i in instance.get_windows()]    
+        
+        # Get the addresses of currently open windows
+        updated_windows = [i.pid for i in instance.get_windows() if i.pid != -1]    
+        
+        # Find the difference between the stored windows and the updated windows
         diff_created = np.setdiff1d(self.stored_windows, updated_windows)
         diff_updated = np.setdiff1d(updated_windows, self.stored_windows)
+        
+        # Combine the differences to check if any window was created or closed
         difference = [np.concatenate((diff_created, diff_updated))]
-        if difference[0]:
-            self.stored_windows = updated_windows
-            return True
+        
+        # Check if there is any difference
+        try:
+            if difference[0].any():
+                # Update the stored windows and return True if there is a difference
+                self.stored_windows = updated_windows
+                return True
+        except Exception as e:
+            print(e)
         else:
+            # No change in windows, return False
             return False
 
-                    
+          
     def Taskbar(self, orientation, class_style, update_button=False, callback=None):
+        # Create an instance of Hyprland to access window information
         instance = Hyprland()
+    
+        # Filter windows to exclude those already in the taskbar
         all_windows = [i for i in instance.get_windows() if i.wm_class and not i.wm_class in self.taskbar_list]
+    
+        # If no new windows, exit the function
         if not all_windows:
             return True
+    
+        # Load configuration from dockbar_config file
         with open(self.dockbar_config, "r") as f:
             config = toml.load(f)
+    
+        # Extract desktop_file paths from the configuration
         launchers_desktop_file = [config[i]["desktop_file"] for i in config]
+    
         for i in all_windows:
             wm_class = i.wm_class.lower()
             address = i.address.lower()
             initial_title = i.initial_title.lower()
             title = i.title
             pid = i.pid
-            if wm_class in launchers_desktop_file and update_button == False:
+    
+            # Skip windows with wm_class found in launchers_desktop_file if update_button is False
+            if wm_class in launchers_desktop_file and not update_button:
                 continue
-            if pid in self.taskbar_list and update_button == False:
+    
+            # Skip windows with pid found in self.taskbar_list if update_button is False
+            if pid in self.taskbar_list and not update_button:
                 continue
-            #quick fix for nautilus initial class
+    
+            # Quick fix for nautilus initial class
             if "org.gnome.nautilus" in wm_class:
                 initial_title = "nautilus"
+    
+            # Create a taskbar launcher button using utility function
             button = self.utils.CreateTaskbarLauncher(wm_class, address, title, initial_title, orientation, class_style)
+    
+            # Append the button to the taskbar
             self.taskbar.append(button)
+    
+            # Store button information in dictionaries for easy access
             self.buttons_pid[pid] = [button, initial_title, address]
             self.buttons_address[address] = [button, title]
-            self.taskbar_list.append(pid)                     
-        return True
     
-
+            # Add the pid to the taskbar_list to keep track of added windows
+            self.taskbar_list.append(pid)
+    
+        # Return True to indicate successful execution of the Taskbar function
+        return True
         
+    
+            
     def update_taskbar(self, pid, wm_class, address, initial_title, title, orientation, class_style, callback=None):
+        # Create a taskbar launcher button using utility function
         button = self.utils.CreateTaskbarLauncher(wm_class, address, title, initial_title, orientation, class_style)
+    
+        # Append the button to the taskbar
         self.taskbar.append(button)
+    
+        # Store button information in dictionaries for easy access
         self.buttons_pid[pid] = [button, initial_title, address]
-        self.buttons_address[address] = [button, title]                  
+        self.buttons_address[address] = [button, title]
+    
+        # Return True to indicate successful execution of the update_taskbar function
         return True
 
     
-    def check_pids(self):   
+    def check_pids(self):
+        # Create an instance of Hyprland
         instance = Hyprland()
-        #*** need a fix since this code depends hyprshell plugin
+    
+        # *** Need a fix since this code depends on the Hyprshell plugin
         if not instance.get_workspace_by_name("OVERVIEW"):
             return True
-        #since I am lazy to verify what could possibly go wrong
-        #then pass exception to always this function return true
-        #so the glib.timeout won't stop to check
+    
+        #do not check anything if no window closed or created
         if not self.is_any_window_created_or_closed():
             return True
+    
         try:
+            # Get the active window and all PIDs of windows with wm_class
             active_window = instance.get_active_window()
             all_pids = [i.pid for i in instance.get_windows() if i.wm_class]
+    
+            # Check if the PIDs have changed
             if all_pids != self.all_pids:
                 self.taskbar_remove()
                 self.all_pids = all_pids
                 self.Taskbar("h", "taskbar")
                 return True
+    
             initial_title = active_window.initial_title
+    
+            # Check if the active window has the title "zsh"
             if initial_title == "zsh":
                 address = active_window.address
                 title = active_window.title
                 wm_class = active_window.wm_class
                 pid = active_window.pid
-                
-                #quick fix for nautilus initial class
+    
+                # Quick fix for nautilus initial class
                 if "org.gnome.nautilus" in wm_class.lower():
                     initial_title = "nautilus"
-
+    
+                # Check if the address is in buttons_address
                 if address in self.buttons_address:
                     addr = self.buttons_address[address]
                     btn = addr[0]
                     btn_title = addr[1]
-                    if title != btn_title :
+    
+                    # Check if the title has changed
+                    if title != btn_title:
                         self.taskbar.remove(btn)
                         self.update_taskbar(pid, wm_class, address, initial_title, title, "h", "taskbar")
-        except:
-            pass  
+    
+        except Exception as e:
+            pass
+    
+        # Return True to indicate successful execution of the check_pids function
         return True
-        
+
 
     def taskbar_remove(self):
+        # Create an instance of Hyprland
         instance = Hyprland()
+    
+        # Get all active PIDs and addresses with wm_class
         all_pids = [i.pid for i in instance.get_windows() if i.wm_class]
         all_addresses = [i.address for i in instance.get_windows() if i.wm_class]
+    
+        # Iterate over copied dictionary to avoid concurrent modification
         for pid in self.buttons_pid.copy():
             button = self.buttons_pid[pid][0]
             address = self.buttons_pid[pid][2]
+    
+            # Check if the PID or address is not in the current list of windows
             if pid not in all_pids and address not in all_addresses:
                 try:
+                    # Remove button and associated data
                     self.taskbar.remove(button)
                     self.taskbar_list.remove(pid)
                     del self.buttons_pid[pid]
-                    del self.buttons_address[address] 
+                    del self.buttons_address[address]
                 except ValueError:
-                    pass  
+                    pass
+    
+        # Return True to indicate successful execution of the taskbar_remove function
         return True
+    
 
                 
     # Append a window to the dockbar
@@ -271,24 +354,6 @@ class Dockbar(Adw.Application):
                 gotoworkspace = f"hyprctl dispatch workspace name:{activeworkspace}".split()
                 call(move_clients)
                 call(gotoworkspace)
-
-    # Launch a docked application
-    def dock_launcher(self, cmd):
-        processes = psutil.process_iter()
-        for process in processes:
-            if process.name() in cmd or cmd in process.name():
-                call(f"hyprctl dispatch workspace name:{cmd}".split())
-                return
-        if ";" in cmd:
-            for line in cmd.split(";"):
-                try:
-                    call(f"hyprctl dispatch workspace name:{cmd}".split())
-                    Popen(line.split(), start_new_session=True)
-                except Exception as e:
-                    print(e)
-        else:
-            call(f"hyprctl dispatch workspace name:{cmd}".split())
-            Popen(cmd.split(), start_new_session=True)
 
     # Initialize Hyprland instance
     def HyprlandInstance(self):
